c9cdfd803d99b4961f7c93d0fc42f8a30e7d6eda
Henrique Gomes Teixeira <henriquegomes080012@gmail.com>
2023-02-26 22:11:29 -0300

    day-1-modulo4

---
+++ fundamentos/bloco-04-introducao-javascript/exercicio3.js
@@ -0,0 +1,13 @@
+let a = 5;
+let b = 10;
+let c = 15;
+
+if (a > b & a > c) {
+    console.log('A é maior');
+}
+else if (b > a & b > c) {
+    console.log('B é maior');
+}
+else {
+    console.log('C é maior');
+}
---
+++ fundamentos/bloco-04-introducao-javascript/variavel.js
@@ -0,0 +1,5 @@
+let myName = 'Henrique';
+let birthCity = 'Porto Velho';
+let birthYear = 2004;
+
+console.log(myName, birthCity, birthYear);
